ad891be28338a32c599e4d79279b47d184b9a8cf
deltasc <deltastrangecharm@gmail.com>
2017-04-29 16:53:27 +0100

    Adding DB link to engine

--- engine/engine.js
+++ engine/engine.js
@@ -1,16 +1,50 @@
 
 
-
 function compareCropWithField(crop,field){
 
-  if(crop.maxHumid<field.maxHumid){
+  if(crop.maxHumid<=field.maxHumid){
     cropVib.issue.push('highHumid')
     //Get difference, need to decide how to weight it
-  } else if(crop.minHumid<field.minHumid) {
+  } else if(crop.minHumid<=field.minHumid) {
     cropVib.issue.push('lowHumid')
     //Get difference, need to decide how to weight it
   }
 
+  if(crop.maxPh<=field.maxPh){
+    cropVib.issue.push('highPh')
+    //Get difference, need to decide how to weight it
+  } else if(crop.minPh<=field.minPh) {
+    cropVib.issue.push('lowPh')
+    //Get difference, need to decide how to weight it
+  }
+
+  if(crop.maxPh<=field.maxPh){
+    cropVib.issue.push('highPh')
+    //Get difference, need to decide how to weight it
+  } else if(crop.minPh<=field.minPh) {
+    cropVib.issue.push('lowPh')
+    //Get difference, need to decide how to weight it
+  }
 
   return cropVib;
+}
+
+function newFarm(name){
+  db.
+
+}
+
+function connectToDB(){
+  var MongoClient = require('mongodb').MongoClient
+  var assert = require('assert');
+
+  // Connection URL
+  var url = 'mongodb://ds123351.mlab.com:23351/heroku_s50rzslz';
+
+  // Use connect method to connect to the server
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  });
+  //db.close();
 }
