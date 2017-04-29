8070ce406a30e744828a3390b9758733d3e54927
deltasc <deltastrangecharm@gmail.com>
2017-04-29 19:18:16 +0100

    changed render to send

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/addcrop', function(request, response) {
 
 app.post('/addcrop', function(request, response) {
   engine.addCrop('testData');
-  response.render('<p>Recived new crop data : '+request.body.cropname+' </p>');
+  response.send('<p>Recived new crop data : '+request.body.cropname+' </p>');
 });
 
 app.get('/input', function(request, response) {
