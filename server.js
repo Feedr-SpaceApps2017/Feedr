0517b2609b73a6eaa9564099c3fcadc8f0c2e613
deltasc <deltastrangecharm@gmail.com>
2017-04-29 18:38:38 +0100

    Moved engine.js into modules folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
-var engine = require('./engine');
+var engine = require('engine');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -54,7 +54,6 @@ app.get('/cool', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 
-  var MongoClient = require('mongodb').MongoClient
   var assert = require('assert');
 
   // Connection URL
