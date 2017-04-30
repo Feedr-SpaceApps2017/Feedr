1d3650194703e0d4318b7c2aee79e11e3bd92f22
deltasc <deltastrangecharm@gmail.com>
2017-04-30 03:11:21 +0100

    compare stuff

--- public/js/compare.js
+++ public/js/compare.js
@@ -1,3 +1,18 @@
+
+
+function addFarmToDisplay(){
+	tbl = d.getElementById("farmTable");
+	num = tbl.dataset.num;
+	num++;
+	tbl.dataset.num = num;
+	div = d.createElement("div");
+	div.className = 'farm';
+	inner = '<p onclick="fieldClick(\'farm#\')" id="farm#">Name</p> <div class="farmNo"><p onclick="fieldClick(\'idNo#\')" id="idNo#">Amnt</p></div>';
+	innerE = inner.replace(/#/g, num);
+	div.innerHTML = innerE;
+	tbl.appendChild(div);
+}
+
 window.onload = function(){
     d = document;
 
