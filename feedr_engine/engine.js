a3ce64694ff84c640cfb5a536fe2f86556623c52
deltasc <deltastrangecharm@gmail.com>
2017-04-30 07:58:15 +0100

    debugging

--- feedr_engine/engine.js
+++ feedr_engine/engine.js
@@ -49,7 +49,7 @@ function compareCropWithField(crop,field){
 function findBestCrop(field,crops){
   results = new Array(0);
   //field = {maxtemp:42,mintemp:15,maxrain:1048.6,minrain:1048.6,maxph:6.8,minph:6.8};
-  console.log(field);
+  console.log(field.maxtemp);
   for (var crop of crops) {
     obj = compareCropWithField(crop,field);
     obj.cropname = crop.cropname;
